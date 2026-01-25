3340eafd85f71c95684af658271f0141cfe0d891
al3xg0r <17903190+al3xg0r@users.noreply.github.com>
2026-01-25 12:38:02 +0200

    Security update: Moved secrets to .env

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
 # config.py
 import os
+from dotenv import load_dotenv
 
-# Получи токен у @BotFather
-BOT_TOKEN = "7571368967:AAHs0mwgP9M7AbjtW3B2UU8Lb2alQD1DjR4"
+# Загружаем переменные из файла .env
+load_dotenv()
 
-# Твой ID в Telegram (узнай у бота @userinfobot), чтобы получать отчеты об ошибках и /support
-ADMIN_ID = 173161322 
+# Получаем данные (если их нет, вернется None)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = os.getenv("ADMIN_ID")
+
+# Преобразуем ID в число, если оно было считано
+if ADMIN_ID:
+    ADMIN_ID = int(ADMIN_ID)
 
 # Пути к файлам
 LOG_FILE = "bot_log.log"
@@ -13,4 +19,4 @@ USER_LANGS_FILE = "data/user_langs.json"
 
 # API URLS
 COINGECKO_URL = "https://api.coingecko.com/api/v3"
-COINCAP_URL = "https://api.coincap.io/v2" # Запасной API
+COINCAP_URL = "https://api.coincap.io/v2"
