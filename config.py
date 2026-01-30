697962d468b6e19a0f706fa7a3deb221bd0b8840
al3xg0r <17903190+al3xg0r@users.noreply.github.com>
2026-01-30 15:02:13 +0200

    Fix config fallback

--- config.py
+++ config.py
@@ -7,11 +7,9 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 
-# Ссылка для Demo API (с ключом)
 COINGECKO_URL = "https://api.coingecko.com/api/v3" 
-COINGECKO_API_KEY = os.getenv("COINGECKO_API_KEY")
+# Добавляем пустую строку по умолчанию, чтобы не было ошибки
+COINGECKO_API_KEY = os.getenv("COINGECKO_API_KEY", "")
 
 COINCAP_URL = "https://api.coincap.io/v2"
-
-# Добавим еще один резерв (не требует ключа для простых запросов)
 CRYPTOCOMPARE_URL = "https://min-api.cryptocompare.com/data/price"
